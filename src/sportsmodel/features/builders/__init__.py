f57ca6a780a18ebbaf4eb6e5484df07602666c9c
Brian Hernandez <304168842+Brianhernandez-dev@users.noreply.github.com>
2026-07-19 14:14:42 -0700

    Add rolling team batting feature builder

--- src/sportsmodel/features/builders/__init__.py
+++ src/sportsmodel/features/builders/__init__.py
@@ -1,7 +1,12 @@
 from sportsmodel.features.builders.base import (
     FeatureBuilder,
 )
+from sportsmodel.features.builders.team_batting import (
+    TeamBattingFeatureBuilder,
+)
+
 
 __all__ = [
     "FeatureBuilder",
+    "TeamBattingFeatureBuilder",
 ]
